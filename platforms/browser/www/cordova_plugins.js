2f217198c462339c5b3b414895a702db1a601f9e
developer-macrew <developer.macrew@gmail.com>
2017-07-12 11:44:03 +0530

    Fixed push notification

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,80 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/at.oneminutedistraction.phonenumber/www/phonenumber.js",
+        "id": "at.oneminutedistraction.phonenumber.PhoneNumber",
+        "pluginId": "at.oneminutedistraction.phonenumber",
+        "clobbers": [
+            "phonenumber"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-calendar/www/Calendar.js",
+        "id": "cordova-plugin-calendar.Calendar",
+        "pluginId": "cordova-plugin-calendar",
+        "clobbers": [
+            "Calendar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-calendar/test/tests.js",
+        "id": "cordova-plugin-calendar.tests",
+        "pluginId": "cordova-plugin-calendar"
+    },
+    {
+        "file": "plugins/cordova-plugin-facebook4/www/facebook-native.js",
+        "id": "cordova-plugin-facebook4.FacebookConnectPlugin",
+        "pluginId": "cordova-plugin-facebook4",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-facebook4/www/facebook-browser.js",
+        "id": "cordova-plugin-facebook4.FacebookConnectPluginBrowser",
+        "pluginId": "cordova-plugin-facebook4",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/src/browser/InAppBrowserProxy.js",
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+        "id": "onesignal-cordova-plugin.OneSignal",
+        "pluginId": "onesignal-cordova-plugin",
+        "clobbers": [
+            "OneSignal"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "at.oneminutedistraction.phonenumber": "0.0.1",
+    "cordova-plugin-calendar": "4.4.2",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-facebook4": "1.7.1",
+    "cordova-plugin-geolocation": "2.2.0",
+    "cordova-plugin-inappbrowser": "1.4.0",
+    "onesignal-cordova-plugin": "2.1.2",
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-console": "1.0.7"
+}
+// BOTTOM OF METADATA
+});
